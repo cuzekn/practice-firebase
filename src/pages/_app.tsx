5d0ac4a4b75c3392c282fb94572e82c7c5ab7dcf
cuze <cuzekn@gmail.com>
2022-05-13 02:20:53 +0900

    feat: TodoList作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+
+export type Todo = {
+  id: number;
+  text: string;
+  isDone: boolean;
+};
+
+const TODOS: Todo[] = [
+  { id: 1, text: "foo", isDone: false },
+  { id: 2, text: "bar", isDone: true },
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
+
+  return <Component {...pageProps} todos={todos} setTodos={setTodos} />;
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,63 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
+import React, { ComponentProps, Dispatch, SetStateAction } from "react";
+import { Header } from "src/component/Layout/Header";
+import { Todo } from "src/pages/_app";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+type Props = {
+  todos: Todo[];
+  setTodos: Dispatch<SetStateAction<Todo[]>>;
 };
 
-export default Home;
+const Index: NextPage<Props> = ({ todos, setTodos }) => {
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = (event) => {
+    event.preventDefault();
+    const text = event.currentTarget.text.value;
+    setTodos((prevTodos) => {
+      const newTodo = { id: prevTodos.length + 1, text, isDone: false };
+      return [...prevTodos, newTodo];
+    });
+    event.currentTarget.reset();
+  };
+
+  const toggleIsDone = (id: Todo["id"]) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isDone: !todo.isDone };
+        }
+        return todo;
+      });
+    });
+  };
+
+  return (
+    <Header title="Home">
+      <div>
+        <h3>TODO追加</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="text" autoComplete="off" required />
+          <button>追加</button>
+        </form>
+      </div>
+
+      <div>
+        <h3>TODO一覧</h3>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <label style={{ fontSize: "2rem" }}>
+              <input
+                type="checkbox"
+                checked={todo.isDone}
+                onChange={() => toggleIsDone(todo.id)}
+                style={{ width: "1.5rem", height: "1.5rem" }}
+              />
+              {todo.text}
+            </label>
+          </div>
+        ))}
+      </div>
+    </Header>
+  );
+};
+
+export default Index;
